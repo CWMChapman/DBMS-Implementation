098adb4d03a8490def82e5c157b53e991ea62154
Christopher Chapman <cwmchapman@gmail.com>
2020-11-11 12:39:05 -0500

    pushing latest hash implementations

--- linearHash.c
+++ linearHash.c
@@ -2,16 +2,54 @@
 
 
 
+/*
+
+FUNCTIONS:
+
+    See rough algorithm in the textbook on page 381
+
+    hash (returns bucket to insert into)
+    insert (returns record)
+    increment_next (void)
+    delete (void)
+    search (look_up) (returns record)
+    split
+
+*/
+
+void increment_next (hash_table* ht) {
+    (*ht).level++;
+    return;
+}
+
+ridPage* getBucketForInsert(hash_table* ht, int key) {
+
+    return;
+}
+
+void insert(hash_table* ht) {
+    
 
 
-// void insert
-
+    return;
+}
 
 // Code to test the hash_table functions
 int main(int argc, char** argv) {
     initPageManager();
 
-    printf("hello");
+    hash_table ht;
+    ht.level = 0;
+    ht.next = &ht.buckets[0];
+    printf("%i\n", ht.level);
+    increment_next(&ht);
+    printf("%i\n", ht.level);
+
+
+
+
+
+
 
 
 
--- linearHash.h
+++ linearHash.h
@@ -3,11 +3,14 @@
 
 #include "pageManager.h"
 
+#define INITIAL_NUM_BUCKETS 4 // This is is the initial number of buckets the hash table will start with.
 
 
 
 typedef struct {
-    
+    int level; // initialized to zero, indicates the current round level and indicates which hash function to use
+    ridPage* next; // initially to the first bucket. pointer to the bucket that will next be split
+    ridPage buckets[(INITIAL_NUM_BUCKETS * sizeof(struct ridPage*)) / sizeof(ridPage)]; // the actual table
 } hash_table;
 
 /*
