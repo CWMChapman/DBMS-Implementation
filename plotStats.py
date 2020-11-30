65d58845c8e9228ab7de18818c700d3eb05a4f97
Christopher Chapman <cwmchapman@gmail.com>
2020-11-30 11:58:37 -0500

    added more plots and stuff

--- plotStats.py
+++ plotStats.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-
+def scanCost(nRecords):
+    return nRecords/7
 
 with open('./Documents/plotRangeSearchStats.txt') as f:
     l = f.readline()
@@ -21,11 +23,12 @@ with open('./Documents/plotRangeSearchStats.txt') as f:
     while (l != "\n"):
         hash_lines.append(l)
         l = f.readline()
-    x_hash = [int(line.strip().split()[0]) for line in hash_lines]
-    y_hash = [int(line.strip().split()[1]) for line in hash_lines]
+    x_hash = np.array([int(line.strip().split()[0]) for line in hash_lines])
+    y_hash = np.array([int(line.strip().split()[1]) for line in hash_lines])
 
-plt.plot(x_tree, y_tree, label="Tree")
+plt.plot(x_tree, y_tree , label="Tree")
 plt.plot(x_hash, y_hash, label="Hash")
+plt.plot(x_hash, scanCost(x_hash), 'r--', label="Scan Cost")
 # plt.yscale("log")
 # plt.xscale("log")
 plt.xlim(right=1E6)
@@ -41,11 +44,30 @@ plt.savefig("./Documents/rangeSearchReadsPlot.png", dpi=300)
 
 plt.clf()
 
+plt.plot(x_tree, np.divide(y_tree, x_tree), label="Tree")
+plt.plot(x_hash, np.divide(y_hash, x_tree), label="Hash")
+plt.plot(x_hash, np.divide(scanCost(x_hash), x_hash), 'r--', label="Scan Cost")
+plt.yscale("log")
+plt.xscale("log")
+plt.xlim(right=1E6)
+# plt.ylim(bottom=0)
+plt.ylim(top=1E6)
+plt.xlabel('Number of Records (log scale)')
+plt.ylabel('Number of Page Reads / Record (log scale)')
+plt.title('Range Search')
+plt.legend()
+# plt.show()
+plt.savefig("./Documents/rangeSearchReadsPerRecordLogPlot.png", dpi=300)
+
+plt.clf()
+
 plt.plot(x_tree, y_tree, label="Tree")
 plt.plot(x_hash, y_hash, label="Hash")
+plt.plot(x_hash, scanCost(x_hash), 'r--', label="Scan Cost")
 # plt.yscale("log")
 plt.xscale("log")
 plt.xlim(right=1E6)
+# plt.xlim(left=0)
 plt.ylim(bottom=0)
 plt.ylim(top=1E6)
 plt.xlabel('Number of Records (log scale)')
@@ -76,19 +98,20 @@ with open('./Documents/plotSearchStats.txt') as f:
     while (l != "\n"):
         hash_lines.append(l)
         l = f.readline()
-    x_hash = [int(line.strip().split()[0]) for line in hash_lines]
-    y_hash = [int(line.strip().split()[1]) for line in hash_lines]
+    x_hash = np.array([int(line.strip().split()[0]) for line in hash_lines])
+    y_hash = np.array([int(line.strip().split()[1]) for line in hash_lines])
 
-plt.plot(x_tree, y_tree, label="Tree")
-plt.plot(x_hash, y_hash, label="Hash")
+plt.plot(x_tree, np.divide(y_tree, x_tree), label="Tree")
+plt.plot(x_hash, np.divide(y_hash, x_hash), label="Hash")
+# plt.plot(x_hash, scanCost(x_hash), 'r--', label="Scan Cost")
 # plt.yscale("log")
 # plt.xscale("log")
-plt.xlim(right=1E6) 
-plt.xlim(left=0) 
-plt.ylim(top=1E6) 
-plt.ylim(bottom=0) #
+# plt.xlim(right=1E6) 
+# plt.xlim(left=0) 
+# plt.ylim(top=1E6) 
+# plt.ylim(bottom=0) #
 plt.xlabel('Number of Records')
-plt.ylabel('Number of Page Reads')
+plt.ylabel('Number of Page Reads / Record')
 plt.title('Search')
 plt.legend()
 # plt.show()
